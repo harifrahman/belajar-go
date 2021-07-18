b257ce259612872680181f46d48fbeb7c05ec963
flaster <harifrahman999@gmail.com>
2021-07-18 11:58:26 +0700

    type data - bool, string

--- number.go
+++ number.go
@@ -6,4 +6,13 @@ func main() {
 	fmt.Println("angka satu", 1)
 	fmt.Println("angka dua", 2)
 	fmt.Println("angka tiga koma lima", 3.5)
+
+	// boolean
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah =", false)
+
+	// string
+	fmt.Println(len("Arif"))
+	fmt.Println("Arif", "Rahman", "Hakim")
+	fmt.Println("Arif Rahman Hakim"[0]) //will output 65, ASCII "A"
 }
